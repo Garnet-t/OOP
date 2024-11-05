de492cc6eff516998b7ee410e4eb5f7dd1bf0231
Garnet-t <145390481+Garnet-t@users.noreply.github.com>
2024-11-06 02:41:58 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,48 +1,33 @@
 #include <iostream>
-int main(){
- /*   Преобразование 12-часового времени, например "8:30 am" или "8:30 pm", в 24-часовое 
-(например, "0830" или "2030")  
- 
-Вам необходимо определить функцию, на вход которой будут поданы час (всегда в диапазоне от 
-1 до 12, включительно), минута (всегда в диапазоне от 0 до 59, включительно) и период (либо 
-"am", либо "pm"). 
- 
-Ваша задача - вернуть четырехсимвольную строку, кодирующую это время в 24-часовом 
-формате. */
-//размер значения указателя (хранимый адрес) не зависит от типа указателя.(хоть int double char)
- 
-// Примечания
-const int max_length {8};
-char time[max_length] {};
-int hour;
-std::cin.getline(time,max_length);
-//std::cin.getline(text, max_length, '!'); символ завершения ввода .не будет включаться в строку
-if (time[1]!=':'){//это 12 
+#include <stdlib.h>
+//void time_need_format(char[],const int);
 
-    if (time[6]=='a'){//полночь 12 am
-        time[0]='0';
-        time[1]='0';
-        //остальное неизменно
+//int main(int argc,char* argv[]){
+//}
+const   int maxlength {9};
+void time_need_format(char time_need1[],const int maxlength){
+    int hour;
+    //const   int maxlength {9};
+    char time_need[maxlength] {};
+    std::cin.getline(time_need,maxlength);
+   // time_need_format(time_need,maxlength);
+    if (time_need[1]=='2' && (time_need[3]!='0' || time_need[4]!='0' )) {exit(1);}
+    if (time_need[6]=='a' ){
+    std::cout<<time_need[0]<<time_need[1]<<time_need[3]<<time_need[4]<<std::endl;
     }
-    // 12 pm полдень
-    else {
-        time[0]='1';
-        time[1]='2';
+  if (time_need[5]=='a' ){
+    std::cout<<"0"<<time_need[0]<<time_need[2]<<time_need[3]<<std::endl;
     }
-}
-else{//все остальные 
-    hour=int(time[0]);
-    if (time[5]=='p')//8 pm=20 
-       hour+=12;
-    //не важно какой часовой пояс
-    time[0]=char(hour/10);
-    time[1]=char(hour%10);
-
-//    std::cout<<time[0]<<time[1]<<time[];
-
-}
-
- std::cout<<time[0]<<time[1]<<time[3]<<time[4]<<std::endl; 
-
-    return 0;
+ if (time_need[6]=='p' ){//11:57 pm=23 57
+    hour=10*(time_need[0] -'0')+time_need[1] - '0' +12;
+    time_need[0]=hour/10 + '0';
+    time_need[1]=hour%10 + '0';
+    std::cout<<time_need[0]<<time_need[1]<<time_need[3]<<time_need[4]<<std::endl;
+    }
+ if (time_need[5]=='p' ){//8:57 pm=20 57
+    hour=time_need[0] - '0'  +12;
+    time_need[0]=hour/10 + '0';
+    time_need[1]=hour%10 + '0';
+    std::cout<<time_need[0]<<time_need[1]<<time_need[2]<<time_need[3]<<std::endl; 
+     }
 }

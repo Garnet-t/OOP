5dda6dd6a9b049d37a6dc5f621331b9e15b08fc6
Garnet-t <145390481+Garnet-t@users.noreply.github.com>
2024-11-06 02:46:11 +0300

    Update lab1.cmake
    delete

--- lab1.cmake
+++ lab1.cmake
@@ -1,12 +1,1 @@
-# Указываем минимальную версию CMake, необходимую для этого проекта
-cmake_minimum_required(VERSION 3.10)
 
-# Определяем имя проекта
-project(Lab1)
-
-# Устанавливаем стандарт C++
-set(CMAKE_CXX_STANDARD 11)
-set(CMAKE_CXX_STANDARD_REQUIRED True)
-
-# Добавляем исходные файлы
-add_executable(lab1 lab1.cpp)

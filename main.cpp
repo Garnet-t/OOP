e5a1b1a5a81002d9ad6a3c25253466893a3569af
Garnet-t <145390481+Garnet-t@users.noreply.github.com>
2024-11-06 02:54:13 +0300

    Update main.cpp
    в /mnt есть  ,в корневой папке на диске С  старая программа

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "lab1.cpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    const int maxlength {9};
+    char time_need1[l];
+    time_need_format(time_need1, maxlength);
     return 0;
 }

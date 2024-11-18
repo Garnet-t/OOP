413b608a5d0617f5edab7d7bc67e2b11c6741296
Garnet-t <145390481+Garnet-t@users.noreply.github.com>
2024-11-18 17:46:30 +0300

    Add files via upload

---
+++ CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProjectLib.dir/lab1.cpp.o"
+  "CMakeFiles/MyProjectLib.dir/lab1.cpp.o.d"
+  "libMyProjectLib.a"
+  "libMyProjectLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProjectLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
